2514d3dc3d6551ee3a1a57025d2f5b62d5173649
lamnaourm <mohammed.lamnaour@gmail.com>
2022-09-26 15:48:39 +0100

    second commit

--- src/component/Counter.jsx
+++ src/component/Counter.jsx
@@ -17,6 +17,8 @@ class Counter extends Component {
 
     this.state = {
       counter: 0,
+      pasInc : 1,
+      pasDe : 1,
     };
 
     this.incrementer = this.incrementer.bind(this);
@@ -25,11 +27,11 @@ class Counter extends Component {
   }
 
   incrementer() {
-    this.setState({ counter: this.state.counter + 1 });
+    this.setState({ counter: this.state.counter + this.state.pasInc });
   }
 
   decrementer() {
-    this.setState({ counter: this.state.counter - 1 });
+    this.setState({ counter: this.state.counter - this.state.pasDe });
   }
 
   initialiser() {
@@ -43,6 +45,22 @@ class Counter extends Component {
           <input type="button" value="Incrementer" onClick={this.incrementer} />
           <input type="button" value="Decrementer" onClick={this.decrementer} />
           <input type="button" value="Initialiser" onClick={this.initialiser} />
+        </div>
+
+        <div className="pasIn">
+            <h2>Pas d'incrementation : {this.state.pasInc}</h2>
+            <input type="button" value="+1" onClick={() => this.setState({pasInc:1})} />
+            <input type="button" value="+2" onClick={() => this.setState({pasInc:2})} />
+            <input type="button" value="+3" onClick={() => this.setState({pasInc:3})} />
+            <input type="button" value="+4" onClick={() => this.setState({pasInc:4})} />
+        </div>
+
+        <div className="pasDe">
+            <h2>Pas de decrementation : {this.state.pasDe}</h2>
+            <input type="button" value="-1" onClick={() => this.setState({pasDe:1})} />
+            <input type="button" value="-2" onClick={() => this.setState({pasDe:2})} />
+            <input type="button" value="-3" onClick={() => this.setState({pasDe:3})} />
+            <input type="button" value="-4" onClick={() => this.setState({pasDe:4})} />
         </div>
       </div>
     );
